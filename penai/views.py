d26e13963f0160ae0366a6e3f5a987296664f21b
Lucky Adogun <meetluckyadogun@gmail.com>
2019-11-07 11:07:05 +0100

    penai removed

--- penai/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-
-def name_gen(request):
-    return render(request, 'penai/namegen.html', {})
